a32f8a70dc3c5fb03a72fd9b49498b002f3470e5
julian <juliandanielfranco@gmail.com>
2020-05-22 21:36:12 -0500

    feat: add new components

--- src/app/components/card-psico/card-psico.component.ts
+++ src/app/components/card-psico/card-psico.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-psico',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardPsicoComponent implements OnInit {
 
+  @Input() psicodelic : any = {};
+  @Input() index:number;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showPsico(){
+    
   }
 
 }
